96826c58db0cccba459345a9d020108675f4e17b
dwi5h <dwish.rezpector@gmail.com>
2023-11-15 16:49:08 +0700

    Add project files.

---
+++ InteriorLoader_SHVDN/Main.cs
@@ -0,0 +1,25 @@
+﻿using GTA;
+using GTA.Native;
+using System.Windows.Forms;
+
+namespace InteriorLoader_SHVDN
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            KeyUp += Main_KeyUp;
+        }
+
+        private void Main_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F1)
+            {
+                Function.Call(Hash.SET_INTERIOR_ACTIVE, 68354, true);
+                Function.Call(Hash.DISABLE_INTERIOR, 68354, false);
+                //Function.Call(Hash.SET_INTERIOR_ACTIVE, 68354, false);
+                GTA.UI.Notification.Show("~g~Interior Loader Mod ~s~Loaded");
+            }
+        }
+    }
+}
